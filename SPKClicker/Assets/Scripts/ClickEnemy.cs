3d2b1bb00e2e0c7dfcf17807adfaf0e4c9586b76
Lgotindev <38589238+Lgotindev@users.noreply.github.com>
2019-02-28 22:16:00 +0600

    Core Create
    I add Core this project

---
+++ SPKClicker/Assets/Scripts/ClickEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickEnemy : MonoBehaviour
+{
+    public Text ScoreText;
+
+    public  int coreScore = 0;
+
+    public void ClickOnEnemy()
+    {
+        coreScore++;
+        ScoreText.text = coreScore.ToString();
+    }
+}
